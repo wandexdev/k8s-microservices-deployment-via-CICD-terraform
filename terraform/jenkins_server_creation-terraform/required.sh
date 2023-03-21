879929044c3fc85f1c2ea11b62d29cd23ade32bd
wandexdev <tiwande04@gmail.com>
2023-03-21 10:18:02 +0100

    Updated required.sh with helm commands

--- terraform/jenkins_server_creation-terraform/required.sh
+++ terraform/jenkins_server_creation-terraform/required.sh
@@ -79,3 +79,8 @@ sudo apt install nodejs -y
 node -v
 sudo apt install npm -y
 npm -v
+
+# Install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
